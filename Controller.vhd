fbf5266dd4badbad6b2eb48fa6133ae17a14c5f2
ltrnnhdng <129613373+ltrnnhdng@users.noreply.github.com>
2025-11-03 14:10:36 +0700

    Refactor exp_controller with additional states and signals
    Added new states and control signals for enhanced functionality.

--- Controller.vhd
+++ Controller.vhd
@@ -1,3 +1,4 @@
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -18,6 +19,7 @@ entity exp_controller is
         out_ld    : out std_logic;
         op_sel    : out std_logic;  -- 1 = add, 0 = sub
         z_op_sel  : out std_logic;  -- chọn phép cộng/trừ cho Z
+        z_sel     : out std_logic;
         done      : out std_logic;
 
         -- debug quan sát FSM
@@ -28,13 +30,13 @@ end entity;
 architecture fsm of exp_controller is
 
     type state_type is (
-        S0, S1, S2, S5, S7, S8, S9, S10, S11, S12, S13, S14, S15
+        S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12
     );
 
     signal state, next_state : state_type;
 
     signal x_ld_int, y_ld_int, z_ld_int, i_ld_int, out_ld_int : std_logic := '0';
-    signal op_sel_int, z_op_sel_int, done_int : std_logic := '0';
+    signal op_sel_int, z_op_sel_int,z_sel_int, done_int : std_logic := '0';
 
 begin
 
@@ -46,6 +48,7 @@ begin
     out_ld   <= out_ld_int;
     op_sel   <= op_sel_int;
     z_op_sel <= z_op_sel_int;
+    z_sel    <= z_sel_int;
     done     <= done_int;
 
     with state select
@@ -85,72 +88,80 @@ begin
         out_ld_int <= '0';
         op_sel_int <= '0';
         z_op_sel_int <= '0';
+        z_sel_int <= '0';
         done_int   <= '0';
 
         case state is
 
             when S0 =>
-                done_int <= '0';
-                next_state <= S1;
+               next_state <= S1;
 
             when S1 =>
                 if start = '1' then
                     next_state <= S2;
                 end if;
 
-            when S2 =>
+            when S2 =>             
+                z_ld_int <= '1';
+                z_sel_int <= '0';
+                next_state <= S3;
+
+            when S3 =>
+                if i_gt_N = '0' then
+	                next_state <= S4; 
+								else
+                  next_state <= S8;
+                end if;
+					
+					--if Z >= 0:
+          --  X_next = X + (Y * 2 ** (-i))
+           -- Y_next = Y + (X * 2 ** (-i))
+           -- Z_next = Z - LUT[i - 1]
+      --  else:
+       --     X_next = X - (Y * 2 ** (-i))
+       --     Y_next = Y - (X * 2 ** (-i))
+        --    Z_next = Z + LUT[i - 1]
+     --   X, Y, Z = X_next, Y_next, Z_next
+            when S4 =>
+                if z_ge_0 = '0' then
+                    next_state <= S5;
+                else
+                    next_state <= S6;
+                end if;
+							
+							
+            when S5 =>
+                op_sel_int <= '0';
+                z_op_sel_int <= '1';
+                next_state <= S7;
+						
+						when S6 =>
+                op_sel_int <= '0';
+                z_op_sel_int <= '1';
+                next_state <= S7;
+                
+            when S7 =>
                 x_ld_int <= '1';
                 y_ld_int <= '1';
                 z_ld_int <= '1';
-                z_op_sel_int <= '0';
-                next_state <= S5;
+                z_sel_int<= '1';
+                i_ld_int <= '1';
+                next_state <= S3;
 
-            when S5 =>
-                if i_gt_N = '1' then
-                    next_state <= S12; 
-                else
-                    next_state <= S7;
-                end if;
-
-            when S7 =>
-                if z_ge_0 = '1' then
-                    op_sel_int <= '0';
-                    z_op_sel_int <= '1';
-                    next_state <= S10;
-                else
-                    next_state <= S9;
-                end if;
+            when S8 =>
+                out_ld_int <= '1';
+                next_state <= S9;
 
             when S9 =>
-                op_sel_int <= '1';
-                z_op_sel_int <= '0';
+                done_int <= '1';
                 next_state <= S10;
 
             when S10 =>
-                x_ld_int <= '1';
-                y_ld_int <= '1';
-                z_ld_int <= '1';
-                z_op_sel_int <= '1';
-                next_state <= S11;
-
-            when S11 =>
-                i_ld_int <= '1';
-                next_state <= S5;
-
-            when S12 =>
-                out_ld_int <= '1';
-                next_state <= S13;
-
-            when S13 =>
-                done_int <= '1';
-                next_state <= S14;
-
-            when S14 =>
                 if start = '0' then
-                    next_state <= S15;
+                    next_state <= S11;
                 end if;
 
-            when S15 =>
+            when S11 =>
                 done_int <= '0';
                 next_state <= S0;
 
@@ -161,3 +172,4 @@ begin
     end process;
 
 end architecture fsm;
+s
