0caafec33ef3e0b40d0fc85e89f75e97e8849e60
ltrnnhdng <129613373+ltrnnhdng@users.noreply.github.com>
2025-11-06 18:27:57 +0700

    Update and rename top_level.vhd to Datapath.vhd

--- source/Datapath.vhd
+++ source/Datapath.vhd
@@ -1,9 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;  -- dùng cho phép toán s? h?c
+use ieee.numeric_std.all;  -- dÃ¹ng cho phÃ©p toÃ¡n s? h?c
 use ieee.STD_LOGIC_UNSIGNED;
 
--- Khai báo entity (giao ti?p)
+-- Khai bÃ¡o entity (giao ti?p)
 entity datapath is
     port (
        rst, clk: in std_logic;
@@ -29,7 +29,7 @@ entity datapath is
     );
 end entity datapath;
 
--- Ki?n trúc bên trong (mô t? ho?t ??ng)
+-- Ki?n trÃºc bÃªn trong (mÃ´ t? ho?t ??ng)
 architecture Behavior of datapath is
 
     -- component declearations 
