226a7e2eddeed5462c97c0217354a4b59f15ec6b
ltrnnhdng <129613373+ltrnnhdng@users.noreply.github.com>
2025-11-14 09:36:36 +0700

    completed with Q4.28 format and increased range to (+-2)

--- source/ultimate_top_module.vhd
+++ source/ultimate_top_module.vhd
@@ -26,17 +26,20 @@ architecture Behavioral of ultimate_top_module is
         z_ge_0    : in  std_logic;
         i_gt_N    : in  std_logic;
 
-        -- control to datapath
-        x_ld      : out std_logic;
-        y_ld      : out std_logic;
-        z_ld      : out std_logic;
-        i_ld      : out std_logic;
-        out_ld    : out std_logic;
-        op_sel    : out std_logic;  -- 1 = add, 0 = sub
-        z_op_sel  : out std_logic;  -- select add/sub for Z
-        z_sel     : out std_logic;
-        done      : out std_logic;
-
+        -- các tín hi?u ?i?u khi?n ra datapath
+        x_ld        : out std_logic;
+        y_ld        : out std_logic;
+        z_ld        : out std_logic;
+        i_ld        : out std_logic;
+        out_ld      : out std_logic;
+        op_sel      : out std_logic;
+        z_op_sel    : out std_logic;
+        z_sel       : out std_logic;
+        done        : out std_logic;
+        xin_ld      : out std_logic; 
+        k_ld        : out std_logic;
+        xtiny_ld    : out std_logic;
+        
         -- debug FSM
         state_reg : out std_logic_vector(3 downto 0);
         reset_ctrl  : out std_logic
@@ -47,8 +50,8 @@ architecture Behavioral of ultimate_top_module is
     port (
        rst, clk :                        in std_logic;
 
-       i_ld, x_ld, y_ld, z_ld, out_ld : in std_logic;   -- FF enable
-
+       i_ld, x_ld, y_ld, z_ld, out_ld,xin_ld, k_ld, xtiny_ld  : in std_logic;
+       
        xy_op_sel, z_op_sel, z_sel:      in std_logic;   -- control ops
 
        i_gt_N, z_ge_0:                  out std_logic;  -- flags
@@ -60,7 +63,7 @@ architecture Behavioral of ultimate_top_module is
   end component;
 
   --=== SIGNALS ===--
-  signal x_ld_top, y_ld_top, z_ld_top, i_ld_top, out_ld_top : std_logic;
+  signal x_ld_top, y_ld_top, z_ld_top, i_ld_top, out_ld_top, xin_ld_top, k_ld_top, xtiny_ld_top : std_logic;
   signal z_ge_0_top, i_gt_N_top : std_logic;
   signal xy_op_sel_top, z_op_sel_top, z_sel_top : std_logic;
   signal done_top : std_logic;
@@ -81,6 +84,9 @@ begin
       y_ld       => y_ld_top,
       z_ld       => z_ld_top,
       out_ld     => out_ld_top,
+      xin_ld     => xin_ld_top, 
+      k_ld       => k_ld_top, 
+      xtiny_ld   => xtiny_ld_top, 
 
       xy_op_sel  => xy_op_sel_top,
       z_op_sel   => z_op_sel_top,
@@ -108,6 +114,10 @@ begin
       z_ld      => z_ld_top,
       i_ld      => i_ld_top,
       out_ld    => out_ld_top,
+      xin_ld     => xin_ld_top, 
+      k_ld       => k_ld_top, 
+      xtiny_ld   => xtiny_ld_top, 
+
 
       op_sel    => xy_op_sel_top,
       z_op_sel  => z_op_sel_top,
