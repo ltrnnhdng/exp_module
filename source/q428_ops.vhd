db70473b4d20e7e81fafebf17a4f8adb5f19c34f
ltrnnhdng <129613373+ltrnnhdng@users.noreply.github.com>
2025-12-19 11:01:07 +0700

    Rename entities and architectures from q214 to q428

--- source/q428_ops.vhd
+++ source/q428_ops.vhd
@@ -3,14 +3,14 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
-entity q214_add is
+entity q428_add is
 Port ( 
     ain, bin: in std_logic_vector (31 downto 0);
     c: out std_logic_vector (31 downto 0)
 );
-end q214_add;
+end q428_add;
 
-architecture Behavioral of q214_add is
+architecture Behavioral of q428_add is
 
 begin
     c <= std_logic_vector(signed(ain) + signed(bin));
@@ -23,14 +23,14 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
-entity q214_sub is
+entity q428_sub is
 Port ( 
     ain, bin: in std_logic_vector (31 downto 0);
     c: out std_logic_vector (31 downto 0)
 );
-end q214_sub;
+end q428_sub;
 
-architecture Behavioral of q214_sub is
+architecture Behavioral of q428_sub is
 begin
     c <= std_logic_vector(signed(ain) - signed(bin));
 end Behavioral;
